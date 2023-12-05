6018ffede635bb14fa3414eea9c131fe2c6e05aa
Mattiatarabolo <mattia.tarabolo@gmail.com>
2023-12-05 16:54:16 +0100

    Fixed types dependencies

--- src/types.jl
+++ src/types.jl
@@ -2,11 +2,11 @@ struct SDEsol
     t::Vector{Float64}
     xs::Array{Float64, 2}
     dt::Float64
-    par::Tuple{Float64, SparseArrays.SparseMatrixCSC{Float64, Int64}, Float64}
+    par::Tuple{Float64, SparseMatrixCSC, Float64}
 end
 
 
-function SDEsol(t::Vector{Float64}, N::Int, dt::Float64, par::Tuple{Float64, SparseArrays.SparseMatrixCSC{Float64, Int64}, Float64})
+function SDEsol(t::Vector{Float64}, N::Int, dt::Float64, par::Tuple{Float64, SparseMatrixCSC, Float64})
     return SDEsol(t, zeros(N, length(t)), dt, par)
 end
 
@@ -15,7 +15,7 @@ struct SDEsim
     t::Vector{Float64}
     xs::Array{Float64, 3}
     dt::Float64
-    pars::Vector{Tuple{Float64, SparseArrays.SparseMatrixCSC{Float64, Int64}, Float64}}
+    pars::Vector{Tuple{Float64, SparseMatrixCSC, Float64}}
     nsim::Int
 end
 
