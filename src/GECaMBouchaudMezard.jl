5983ecf538993f470b42079d8e66dc42705dc311
Mattiatarabolo <mattia.tarabolo@gmail.com>
2023-12-06 16:34:51 +0100

    prog bar

--- src/GECaMBouchaudMezard.jl
+++ src/GECaMBouchaudMezard.jl
@@ -6,7 +6,7 @@ using StatsBase, CurveFit
 using Graphs
 using Plots, StatsPlots
 using JLD2
-using StaticArrays
+import ProgressMeter.@showprogress
 
 export BM_MilSDE, sim_BM_MilSDE, BM_MilSDE_JLD, sim_BM_MilSDE_JLD, idx_t, pareto_fit, pdf_norm_wealth, pareto_exponent
 
