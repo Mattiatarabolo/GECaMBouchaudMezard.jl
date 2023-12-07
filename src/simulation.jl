a8acd0dbe3d2d674fc67be64cf33ad6d4bef540d
Mattiatarabolo <mattia.tarabolo@gmail.com>
2023-12-07 16:08:04 +0100

    Minor fix

--- src/simulation.jl
+++ src/simulation.jl
@@ -10,7 +10,7 @@ g_Mil!(dx::Vector{Float64}, x::Vector{Float64}, p::Tuple{Float64, SparseMatrixCS
 
 function Mil_update!(x::Vector{Float64}, Δx_det::Vector{Float64}, Δx_stoch::Vector{Float64}, Δx_Mil::Vector{Float64}, ΔW::Vector{Float64}, dt::Float64, N::Int)
     @turbo for i in 1:N
-        x[i] = x[i] + Δx_det[i]*dt + Δx_stoch[i]*ΔW[i]*sqrt(dt) + Δx_Mil[i]*(ΔW[i].^2 - 1)*dt
+        x[i] = x[i] + Δx_det[i]*dt + Δx_stoch[i]*ΔW[i]*sqrt(dt) + Δx_Mil[i]*(ΔW[i]^2 - 1)*dt
     end
 end
 
