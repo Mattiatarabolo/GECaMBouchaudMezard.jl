d4df7f8449e0d1b4d480972bda6df2d872959b7b
Mattiatarabolo <mattia.tarabolo@gmail.com>
2023-12-08 16:44:22 +0100

    Fixed bugs

--- src/utils.jl
+++ src/utils.jl
@@ -96,7 +96,7 @@ function pareto_fit(sim::SDEsim, ts::UnitRange{Int64}, nbins::Int, frac_init::Fl
 end
 =#
 
-function save_JLD(xs::Matrix{Float64}, N::Int, K::Int, J::Float65, σ²::Float64, dt::Float64, t_end::Float64)
+function save_JLD(xs::Matrix{Float64}, N::Int, K::Int, J::Float64, σ²::Float64, dt::Float64, t_end::Float64)
 
     dirpath = "./data/single_sol/sol"
     mkpath(dirpath)
@@ -105,14 +105,14 @@ function save_JLD(xs::Matrix{Float64}, N::Int, K::Int, J::Float65, σ²::Float64
  end
 
 
-function save_JLD(xs::Matrix{Float64}, N::Int, K::Int, J::Float65, σ²::Float64, dt::Float64, t_end::Float64, idx_sim::Int, dirpath::String)
+function save_JLD(xs::Matrix{Float64}, N::Int, K::Int, J::Float64, σ²::Float64, dt::Float64, t_end::Float64, idx_sim::Int, dirpath::String)
 
     @save dirpath*"/sol_N-$(N)_K$(K)_J-$(J)_s2-$(σ²)_dt-$(dt)_T-$(t_end)_$(idx_sim).jld" xs
 
 end
 
 
-function save_JLD(xs_sim::Array{Float64, 3}, N::Int, K::Int, J::Float65, σ²::Float64, dt::Float64, t_end::Float64)
+function save_JLD(xs_sim::Array{Float64, 3}, N::Int, K::Int, J::Float64, σ²::Float64, dt::Float64, t_end::Float64)
 
     dirpath = "./data/sim/sim"
 
