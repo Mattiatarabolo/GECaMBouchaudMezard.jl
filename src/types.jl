2a9f527ce768e4a0c7f264be800d6a5ec6c119d6
Mattiatarabolo <mattia.tarabolo@gmail.com>
2024-03-19 15:53:43 +0100

    Incremental compilation fixed

--- src/types.jl
+++ src/types.jl
@@ -3,12 +3,13 @@ struct SDEsol
     xs::Array{Float64, 2}
     dt::Float64
     par::Tuple{Float64, SparseMatrixCSC, Float64}
+
+    function SDEsol(t::Vector{Float64}, N::Int, dt::Float64, par::Tuple{Float64, SparseMatrixCSC, Float64})
+        new(t, zeros(N, length(t)), dt, par)
+    end
 end
 
 
-function SDEsol(t::Vector{Float64}, N::Int, dt::Float64, par::Tuple{Float64, SparseMatrixCSC, Float64})
-    return SDEsol(t, zeros(N, length(t)), dt, par)
-end
 
 
 struct SDEsim
@@ -17,9 +18,11 @@ struct SDEsim
     dt::Float64
     pars::Vector{Tuple{Float64, SparseMatrixCSC, Float64}}
     nsim::Int
+
+    function SDEsim(t::Vector{Float64}, N::Int, dt::Float64, nsim::Int)
+        new(t, zeros(nsim, N, length(t)), dt, [(0.0, zeros(N, N), 0.0) for _ in 1:nsim], nsim)
+    end
 end
 
 
-function SDEsim(t::Vector{Float64}, N::Int, dt::Float64, nsim::Int)
-    return SDEsim(t, zeros(nsim, N, length(t)), dt, [(0.0, zeros(N, N), 0.0) for _ in 1:nsim], nsim)
-end
+
